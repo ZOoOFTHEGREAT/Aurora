94c3d8e15030cfb413981c0634403f5d42b18f8d
Aragabg <149136596+Aragabg@users.noreply.github.com>
2023-11-08 21:06:25 +0200

    BLL Arch just files By Ragab

---
+++ AuroraBLL/Dtos/UserDtos/ReadUserByIdDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraBLL.Dtos.UserDtos
+{
+    internal class ReadUserByIdDto
+    {
+    }
+}
