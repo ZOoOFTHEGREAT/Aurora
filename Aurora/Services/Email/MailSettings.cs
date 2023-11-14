630fbef621ab2ca9b4b7ff174d9ecf6745e89c42
Aragabg <149136596+Aragabg@users.noreply.github.com>
2023-11-14 07:38:30 +0200

    Adding Regular Expression on Email in MailService By Ragab

--- Aurora/Services/Email/MailSettings.cs
+++ Aurora/Services/Email/MailSettings.cs
@@ -1,7 +1,10 @@
-﻿namespace AuroraAPI.Services.Email
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuroraAPI.Services.Email
 {
     public class MailSettings
     {
+        [RegularExpression(@"^[a-zA-Z0-9]+@.*\.com")]
         public string Email { get; set; } = string.Empty;
         public string DisplayName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
