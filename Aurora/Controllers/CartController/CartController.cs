4af1e0978ecf18da6fdd1968bea866341305828e
Mohammed Zareef Abotaleb <140671873+ZOoOFTHEGREAT@users.noreply.github.com>
2023-11-17 01:40:58 +0200

    Ali's Controllers Added

---
+++ Aurora/Controllers/CartController/CartController.cs
@@ -0,0 +1,60 @@
+﻿using AuroraBLL.Dtos;
+using AuroraBLL.Dtos.CartDtos;
+using AuroraBLL.Dtos.OrderItemDtos;
+using AuroraBLL.Managers.CartManager;
+using AuroraBLL.Managers.OrderItemManager;
+using AuroraDAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuroraAPI.Controllers.CartController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        #region Inject
+        private readonly ICartManager _cartManager;
+
+        public CartController(ICartManager cartManager) {
+            _cartManager = cartManager;
+
+        }
+        #endregion
+
+        #region Add Cart
+        [HttpPost]
+
+        public ActionResult Add(AddCartDto addCartDto) {
+            if (addCartDto is null)
+            {
+                return NotFound(addCartDto);
+            }
+
+            var AddedCart = _cartManager.AddCart(addCartDto);
+            return CreatedAtAction(nameof(Add), AddedCart);
+        }
+        #endregion
+
+        #region Get All
+        [HttpGet]
+        public ActionResult<List<ReadCartDto>> GetAll() {
+
+            return _cartManager.GetAll().ToList();
+        }
+        #endregion
+
+        #region Get By User Id
+        [HttpGet]
+        [Route("{userId}")]
+        public ActionResult<ReadCartDetailDto> GetByUserId(String userId) {
+            ReadCartDetailDto? readCartDetailDto = _cartManager.GetCartByUserId(userId);
+            if (readCartDetailDto is null) { 
+                return NotFound(readCartDetailDto);
+            
+            }
+            return Ok(readCartDetailDto);
+        }
+        #endregion
+    }
+}
--- Aurora/obj/Debug/net7.0/AuroraAPI.AssemblyInfo.cs
+++ Aurora/obj/Debug/net7.0/AuroraAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuroraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b18c6b4ab136ec40238b67f20d84846311486c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuroraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuroraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
